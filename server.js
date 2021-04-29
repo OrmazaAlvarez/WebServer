94d3580a4cec7d1f8b3d622dd4ec8381e6afc0ec
Lenin Antonio Ormaza Alvarez <lenin.ormaza@gmail.com>
2021-04-29 17:52:56 -0500

    Cargando una pagina web completa en mi servidor

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
+require('dotenv').config();
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
+const port = process.env.PORT;
 
-app.use(express.static(__dirname + '/public'));
-
-//Agregando a Express HBS engine 
 app.set('view engine', 'hbs');
 
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use(express.static('public'));
+
+//Agregando a Express HBS engine 
+
 app.get('/', (req, res) => {
-    res.render('home', {
-        nombre: 'Lenin',
-        anio: new Date().getFullYear()
-    });
+  res.render('home', {
+    name: 'Lenin',
+    title: 'Learning Node',
+    year: new Date().getFullYear()
+  });
 });
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 8080');
+
+app.get('/generic', (req, res) => {
+  res.render('generic', {
+    name: 'Lenin',
+    title: 'Learning Node',
+    year: new Date().getFullYear()
+  });
+});
+
+app.get('/elements', (req, res) => {
+  res.render('elements', {
+    name: 'Lenin',
+    title: 'Learning Node',
+    year: new Date().getFullYear()
+  });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/404.html');
+});
+
+app.listen(port, () => {
+  console.log(`Escuchando peticiones en el puerto ${port}`);
 });
